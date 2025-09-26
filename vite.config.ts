8b9319e5c635d63c0069a4e2e644bc70f0dc7ea1
Facundo Galleguillo <facu.galleguilo@gmail.com>
2025-09-26 12:28:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/gurps-4th-edition-online-sheet/',
   server: {
     host: "::",
     port: 8080,
